07e5e2958d9bc99f1c60bf7676a33ae09cf557ad
Leonardo Aldarondo <leonardo.aldarondo@gmail.com>
2022-09-13 18:10:38 -0400

    end of D0

--- Earthquake_Challenge/static/js/logic_step_5.js
+++ Earthquake_Challenge/static/js/logic_step_5.js
@@ -61,7 +61,8 @@ let overlays = {
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps, overlays, {
-    collapsed: false}
+    collapsed: false
+}
 ).addTo(map);
 
 
@@ -135,5 +136,36 @@ d3.json(earthquakeData).then(function (data) {
         }
 
     }).addTo(earthquakes);
+
+    // Create a legend control object.
+    let legend = L.control({
+        position: "bottomright"
+    });
+
+    // Then add all the details for the legend.
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
+
+        const magnitudes = [0, 1, 2, 3, 4, 5];
+        const colors = [
+            "#98ee00",
+            "#d4ee00",
+            "#eecc00",
+            "#ee9c00",
+            "#ea822c",
+            "#ea2c2c"
+        ];
+
+        // Looping through our intervals to generate a label with a colored square for each interval.
+        for (var i = 0; i < magnitudes.length; i++) {
+            console.log(colors[i]);
+            div.innerHTML +=
+                "<i style='background: " + colors[i] + "'></i> " +
+                magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
+
+    legend.addTo(map);
     // no data beyond this point
 });
